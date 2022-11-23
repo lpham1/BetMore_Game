879d93bebb7d95c602ed0a94c8830266c5f9f236
Linh Khanh <lpham1@smail.uni-koeln.de>
2022-11-23 11:03:43 +0100

    add tie case

--- src/Game_Chair.java
+++ src/Game_Chair.java
@@ -1,17 +1,18 @@
 public class Game_Chair {
 
-    public static void start(Player player){
+    public static void start(Player player) {
         player.play();
     }
-    public static void findWinner(Player p1, Player p2){
-        System.out.println("Computer picked "+ p1.getPicked_num());
-        System.out.println("Player 2 picked "+ p2.getPicked_num());
-        if(p1.getPicked_num() > p2.getPicked_num()){
-           System.out.println("Result: Computer won!");
-        }
-        else{
-            System.out.println("Result: Player 2 won!");
-        }
 
+    public static void findWinner(Player p1, Player p2) {
+        System.out.println("Computer picked " + p1.getPicked_num());
+        System.out.println("Player 2 picked " + p2.getPicked_num());
+        if (p1.getPicked_num() > p2.getPicked_num()) {
+            System.out.println("Result: Computer won!");
+        } else if (p1.getPicked_num() < p2.getPicked_num()) {
+            System.out.println("Result: Player 2 won!");
+        } else {
+            System.out.println("Result: It's a tie");
+        }
     }
 }
--- src/Shuffle.java
+++ src/Shuffle.java
@@ -4,7 +4,7 @@ public class Shuffle {
 
     public static int generateNumber(){
         Random rand = new Random();
-        int int_random = rand.nextInt(101);
+        int int_random = rand.nextInt(101)+1;
         return int_random;
     }
 }
